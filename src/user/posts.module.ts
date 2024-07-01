a9973729b8e30403a7531d5ef0091c2dde123bf3
levanthang23092002 <levanthang230902@gmail.com>
2024-07-01 15:43:00 +0700

    homework 80

--- src/user/posts.module.ts
+++ src/user/posts.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { PostsController } from './user.controller';
 import { PostsService } from './posts.service';
+import { LoggerMiddleware } from '../logger/middleware';
 
 @Module({
   controllers: [PostsController],
   providers: [PostsService],
 })
-export class PostsModule {}
+export class PostsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes(PostsController); // Áp dụng middleware cho tất cả các routes trong PostsController
+  }
+}
